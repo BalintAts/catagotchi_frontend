8fb8dda400dd20635e9a5dfd635e8f1086eb32b3
Bálint Áts <atsbalint@gmail.com>
2020-05-24 16:31:24 +0200

    cats are displayed in (material ui)grid

--- src/components/catGrid.jsx
+++ src/components/catGrid.jsx
@@ -1,15 +1,29 @@
 import React, { Component } from 'react'
 import { fakeData } from '../fakeData/fakeData';
 import CatCard from './catCard';
+import { Grid } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
 
 const CatGrid = props => {
     return (
-        <ul> {fakeData.map(cat =>
-            <li key={cat.id}>
-                <CatCard name={cat.name} img={cat.img} />
-            </li>
-        )}
-        </ul>
+        // <ul> {fakeData.map(cat =>
+        //     <li key={cat.id}>
+        //         <CatCard name={cat.name} img={cat.img} />
+        //     </li>
+        // )}
+        // </ul>
+
+
+
+        <Grid container
+            spacing={4}
+            justify="center">
+            {fakeData.map(cat =>
+                <Grid item xs={12} sm={6} md={4} key={cat.id}>
+                    <CatCard name={cat.name} img={cat.img} />
+                </Grid>
+            )}
+        </Grid>
     );
 };
 
--- src/fakeData/fakeData.jsx
+++ src/fakeData/fakeData.jsx
@@ -1,7 +1,7 @@
 export const fakeData = [
     { id: 0, name: 'Purry', img: "https://d2ph5fj80uercy.cloudfront.net/04/cat2972.jpg" },
     { id: 1, name: 'Schrödinger', img: "https://d2ph5fj80uercy.cloudfront.net/06/cat1996.jpg" },
-    { id: 2, name: 'Lukrécia', img: "https://d2ph5fj80uercy.cloudfront.net/06/cat2630.jpg" },
+    { id: 2, name: 'Ópamacs', img: "https://d2ph5fj80uercy.cloudfront.net/06/cat2630.jpg" },
     { id: 3, name: 'Guriga', img: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQDUBhnLKerrtsNVx1l-QiaPrnJfG5zNFY-O5F_Xc9XNNJgukrj&usqp=CAU" },
-    { id: 4, name: 'Ópamacs', img: "https://www.stylist.co.uk/images/app/uploads/2018/10/11175734/kazuky-akayashi-703909-unsplash-256x256.jpg?w=256&h=256&fit=max&auto=format%2Ccompress" }
+    { id: 4, name: 'Lukrécia', img: "https://www.stylist.co.uk/images/app/uploads/2018/10/11175734/kazuky-akayashi-703909-unsplash-256x256.jpg?w=256&h=256&fit=max&auto=format%2Ccompress" }
 ];

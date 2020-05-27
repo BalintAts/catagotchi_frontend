bc1d093e1759016d409c2dcffcae9f6e74723e06
Bálint Áts <atsbalint@gmail.com>
2020-05-27 13:02:17 +0200

    add h1 dynamic h1 title in CatGrid component

--- src/components/catGrid.jsx
+++ src/components/catGrid.jsx
@@ -8,9 +8,24 @@ import { FilterProvider } from './searchAttributes/filterContext';
 const CatGrid = props => {
     const [filters, setFilters] = useContext(FilterContext);
     const [filteredData, setFilteredData] = useState(fakeData);
-
+    const [isLoading, setIsLoading] = useState(false);
+    const [title, setTitle] = useState("All Cats");
 
     useEffect(() => {
+        if (props.isUsersCats) {
+            setTitle("My Cats")
+            const url = "backednUrl";
+            fetch(url)
+                .then(response => response.json())
+                .then(data => {
+                    console.log(data.results);
+                    setFilteredData(data.results);
+                    setIsLoading(false);
+                })
+                .catch(error => console.log(error));
+
+
+        }
         const filteredData = fakeData;
         if (filters.gender !== "any") {
             setFilteredData(filteredData.filter(cat => cat.gender === filters.gender));
@@ -19,24 +34,28 @@ const CatGrid = props => {
             setFilteredData(filteredData.filter(cat => cat.gender === filters.age));
         }
         console.log("catgrid useeffect");
+
+
+
         // console.log(filters);
-    }, [filters]);
+    }, [filters, props.isUsersCats]);
 
 
     return (
 
         <FilterProvider>
             <>
-                <Grid container
-                    spacing={4}
-                    justify="center">
-                    {filteredData.map(cat =>
-                        <Grid item xs={12} sm={6} md={4} key={cat.id}>
-                            <CatCard name={cat.name} img={cat.img} />
-                        </Grid>
-                    )}
-                </Grid>
-                {/* <h1>{value}</h1> */}
+                <h1>{title}</h1>
+                {isLoading ? (<h3>Loading...</h3>) :
+                    <Grid container
+                        spacing={4}
+                        justify="center">
+                        {filteredData.map(cat =>
+                            <Grid item xs={12} sm={6} md={4} key={cat.id}>
+                                <CatCard name={cat.name} img={cat.img} />
+                            </Grid>
+                        )}
+                    </Grid>}
             </>
         </FilterProvider >
     );

e2d8c4e3f4757f5bf3ab305b07e26e2db035949a
Katalin Nemeth <zeoramail@gmail.com>
2020-06-04 12:05:59 +0200

    Json data

--- src/components/catGrid.jsx
+++ src/components/catGrid.jsx
@@ -1,67 +1,72 @@
-import React, { useEffect, useState, useContext } from 'react'
-import { fakeData } from '../fakeData/fakeData';
-import CatCard from './catCard';
+import React, { useEffect, useState, useContext } from "react";
+import { fakeData } from "../fakeData/fakeData";
+import CatCard from "./catCard";
 import { Grid } from "@material-ui/core";
-import { FilterContext } from './searchAttributes/filterContext';
-import { FilterProvider } from './searchAttributes/filterContext';
-import '../styles/catGrid.css';
+import { FilterContext } from "./searchAttributes/filterContext";
+import { FilterProvider } from "./searchAttributes/filterContext";
+import "../styles/catGrid.css";
 import axios from "axios";
 
+const CatGrid = (props) => {
+  const [filters, setFilters] = useContext(FilterContext);
+  // console.log(filters);
+  const [filteredData, setFilteredData] = useState(fakeData);
+  const [isLoading, setIsLoading] = useState(true);
+  const [title, setTitle] = useState("All Cats");
 
-const CatGrid = props => {
-    const [filters, setFilters] = useContext(FilterContext);
-    // console.log(filters);
-    const [filteredData, setFilteredData] = useState(fakeData);
-    const [isLoading, setIsLoading] = useState(true);
-    const [title, setTitle] = useState("All Cats");
+  useEffect(() => {
+    console.log("useffect called");
+    if (props.isUsersCats) {
+      setTitle("My Cats");
+    }
 
-    useEffect(() => {
-        console.log("useffect called");
-        if (props.isUsersCats) {
-            setTitle("My Cats")
-        }
+    axios
+      .get(`http://localhost:8080/my-cats/`)
+      .then((resp) => {
+        console.log(resp);
+        setFilteredData(resp.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
 
-        // axios.get(`http://localhost:8080/my-cats/`)
-        //     .then(resp => {
-        //         console.log(resp.data);
-        //         setFilteredData(resp.data);
+    const filteredData = fakeData;
+    if (filters.gender !== "any") {
+      setFilteredData(
+        filteredData.filter((cat) => cat.gender === filters.gender)
+      );
+    }
+    if (filters.age !== "any") {
+      setFilteredData(filteredData.filter((cat) => cat.age === filters.age));
+    }
+    console.log("catgrid useeffect");
+    setIsLoading(false);
+  }, [
+    filters.age,
+    filters.gender,
+    setFilters.age,
+    setFilters.gender,
+    props.isUsersCats,
+  ]);
 
-        //     })
-        //     .catch(error => {
-        //         console.log(error)
-        //     });
-
-
-        const filteredData = fakeData;
-        if (filters.gender !== "any") {
-            setFilteredData(filteredData.filter(cat => cat.gender === filters.gender));
-        }
-        if (filters.age !== "any") {
-            setFilteredData(filteredData.filter(cat => cat.age === filters.age));
-        }
-        console.log("catgrid useeffect");
-        setIsLoading(false);
-    }, [filters.age, filters.gender, setFilters.age, setFilters.gender, props.isUsersCats]);
-
-
-    return (
-        <FilterProvider>
-            <>
-                <h1>{title}</h1>
-                {isLoading ? (<h3>Loading...</h3>) :
-                    <Grid container
-                        spacing={4}
-                        justify="center">
-                        {filteredData.map(cat =>
-                            <Grid item xs={12} sm={6} md={4} key={cat.id}>
-                                <CatCard id={cat.id} name={cat.name} img={cat.img} />
-                            </Grid>
-                        )}
-                    </Grid>}
-            </>
-        </FilterProvider >
-    );
+  return (
+    <FilterProvider>
+      <>
+        <h1>{title}</h1>
+        {isLoading ? (
+          <h3>Loading...</h3>
+        ) : (
+          <Grid container spacing={4} justify="center">
+            {filteredData.map((cat) => (
+              <Grid item xs={12} sm={6} md={4} key={cat.id}>
+                <CatCard id={cat.id} name={cat.name} img={cat.img} />
+              </Grid>
+            ))}
+          </Grid>
+        )}
+      </>
+    </FilterProvider>
+  );
 };
-
 
 export default CatGrid;
--- src/components/sideSection.jsx
+++ src/components/sideSection.jsx
@@ -1,55 +1,54 @@
-import React, { useContext } from 'react';
-import '../styles/sideSectionStyle.scss';
-import { SidebarContext } from './sideBar';
-import { FilterContext } from './searchAttributes/filterContext';
-import { FilterProvider } from './searchAttributes/filterContext';
-
-
+import React, { useContext } from "react";
+import "../styles/sideSectionStyle.scss";
+import { SidebarContext } from "./sideBar";
+import { FilterContext } from "./searchAttributes/filterContext";
+import { FilterProvider } from "./searchAttributes/filterContext";
 
 const SideSection = () => {
-    const [filters, setFilters] = useContext(FilterContext);
+  const [filters, setFilters] = useContext(FilterContext);
 
+  const handleGenderChange = (event) => {
+    let newGender = event.target.value;
+    setFilters(Object.assign(filters, { gender: newGender }));
+  };
 
-    const handleGenderChange = event => {
-        let newGender = event.target.value;
-        setFilters(Object.assign(filters, { gender: newGender }));
-    }
+  const handleAgeChange = (event) => {
+    let newAge = event.target.value;
+    setFilters(Object.assign(filters, { age: newAge }));
+  };
 
-    const handleAgeChange = event => {
-        let newAge = event.target.value;
-        setFilters(Object.assign(filters, { age: newAge }));
-    }
-
-    const { isShowSidebar, setIsShowSidebar } = useContext(SidebarContext);
-    return (
-
-        <FilterProvider>
-            <div className={`Sidebar_sideSection    sideSection--${isShowSidebar ? 'show' : 'hide'}`}>
-                <div className="sideSection_topWrapper">
-                    <button onClick={() => setIsShowSidebar(false)}>Hide</button>
-                </div>
-                <div className="sideSection_menu_Wrapper">
-                    <li>
-                        <p>Gender:</p>
-                        {/* <Select options={genderOptions} onInputChange={setGender} /> */}
-                        <select value={filters.gender} onChange={handleGenderChange}>
-                            <option value="any">any</option>
-                            <option value="male">male</option>
-                            <option value="female">female</option>
-                        </select>
-                        <p>Age:</p>
-                        <select value={filters.age} onChange={handleAgeChange}>
-                            <option value="any">any</option>
-                            <option value="young">young</option>
-                            <option value="adult">adult</option>
-                            <option value="senior">senior</option>
-                        </select>
-                    </li>
-                </div>
-            </div>
-        </FilterProvider>
-    );
-}
-
+  const { isShowSidebar, setIsShowSidebar } = useContext(SidebarContext);
+  return (
+    <FilterProvider>
+      <div
+        className={`Sidebar_sideSection    sideSection--${
+          isShowSidebar ? "show" : "hide"
+        }`}
+      >
+        <div className="sideSection_topWrapper">
+          <button onClick={() => setIsShowSidebar(false)}>Hide</button>
+        </div>
+        <div className="sideSection_menu_Wrapper">
+          <li>
+            <p>Gender:</p>
+            {/* <Select options={genderOptions} onInputChange={setGender} /> */}
+            <select value={filters.gender} onChange={handleGenderChange}>
+              <option value="any">any</option>
+              <option value="male">male</option>
+              <option value="FEMALE">female</option>
+            </select>
+            <p>Age:</p>
+            <select value={filters.age} onChange={handleAgeChange}>
+              <option value="any">any</option>
+              <option value="young">young</option>
+              <option value="adult">adult</option>
+              <option value="senior">senior</option>
+            </select>
+          </li>
+        </div>
+      </div>
+    </FilterProvider>
+  );
+};
 
 export default SideSection;

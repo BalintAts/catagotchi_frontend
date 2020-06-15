15b64957191a3accac48083462c9e0a955525553
Bálint Áts <atsbalint@gmail.com>
2020-06-15 15:55:35 +0200

    Add placeholder arrow image from static

--- src/pages/catDetail.jsx
+++ src/pages/catDetail.jsx
@@ -3,6 +3,8 @@ import '../styles/catCard.css';
 import axios from "axios";
 import { IsUsersCatsContext, IsUsersCatsProvider } from "../contexts/isUsersCats_context";
 import { Grid } from '@material-ui/core';
+import Arrow from './arrow.jpg';
+
 
 
 
@@ -91,7 +93,7 @@ const CatDetail = props => {
             {isUsersCats ? (<h2>This is one of my cats</h2>) : (<h2>This is not my cat</h2>)}
             {isLoading ? (<h3> Loading...</h3 >) :
                 <Grid container justify="space-around">
-                    <Grid contatiner item direction="column" justify="space-between">
+                    <Grid contatiner item direction="column" justify="space-between" xs={4}>
                         <Grid item style={{ margin: "30px" }}>
                             <img className="card" src={img} alt={img}></img>
                         </Grid>
@@ -101,10 +103,15 @@ const CatDetail = props => {
 
                         {isUsersCats === false && (<button onClick={adopt}>adopt</button>)}
                     </Grid>
-                    <Grid item>
-                        <img src={"https://www.pinclipart.com/picdir/big/372-3723662_png-cat-emoticon-07-cute-cat-emote-png.png"} alt={"https://www.pinclipart.com/picdir/big/372-3723662_png-cat-emoticon-07-cute-cat-emote-png.png"} width="500" height="600"></img>
+                    <Grid container item direction="column" xs={4}>
+                        <Grid item>
+                            <img src="https://www.pinclipart.com/picdir/big/372-3723662_png-cat-emoticon-07-cute-cat-emote-png.png" alt="https://www.pinclipart.com/picdir/big/372-3723662_png-cat-emoticon-07-cute-cat-emote-png.png" width="500" height="600"></img>
+                        </Grid>
+                        <Grid item>
+                            <img src={Arrow} alt={Arrow} width="250" height="250"></img>
+                        </Grid>
                     </Grid>
-                    <Grid contatiner item direction="column" justify="space-around">
+                    <Grid contatiner item direction="column" justify="space-around" xs={4}>
                         <Grid item>
                             {isUsersCats && (<button onClick={feed}>Feed</button>)}
                             {isHungry && isHungry ? (<h2>Hungry!</h2>) : (<h2>not hungry</h2>)}
